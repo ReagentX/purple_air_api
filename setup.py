d500c8d77e8fab7c051c535ae55e8a2c1f90e968
Christopher Sardegna <github@reagentx.net>
2018-11-01 23:08:22 -0700

    Update setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+
+setup(
+    name='puepleair_api',
+    version='1.0',
+    description='A Python 3.7 PurpleAir API Class.',
+    author='Christopher Sardegna',
+    author_email='github@reagentx.net',
+    scripts=[]
+)
