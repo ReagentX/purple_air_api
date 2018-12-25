a5d3738e91051a21d20a7885ce476e2a65330029
Christopher Sardegna <github@reagentx.net>
2018-12-25 11:20:06 -0800

    Add some test code

--- scripts/run.py
+++ scripts/run.py
@@ -3,3 +3,8 @@ from purpleair import purpleair
 
 p = purpleair.PurpleAir()
 print(len(p.useful_sensors))
+s = p.useful_sensors[0]
+s.get_field('field2')
+s.get_location()
+# print(s.thingspeak_data)
+print(s)
