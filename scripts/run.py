7b465e4051ca28204eceb4946e2a9bca2f3f6ce7
Christopher Sardegna <github@reagentx.net>
2018-12-25 12:18:09 -0800

    Add comments

--- scripts/run.py
+++ scripts/run.py
@@ -2,13 +2,16 @@ from purpleair import purpleair
 from purpleair import sensor
 
 
+# All Sensors
 p = purpleair.PurpleAir()
 print(len(p.useful_sensors))
-s = p.useful_sensors[0]
+s = p.useful_sensors[0] # First confirmed useful sensor
 s.get_field('field2')
 # s.get_location()
 print(s)
 
+
+# Single sensor
 se = sensor.Sensor('2891', parse_location=True)
 se.get_field('field3')
 se.get_field('field4')
