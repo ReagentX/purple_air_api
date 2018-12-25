5220c7ab723813fe747c3419e8c3e10be93f6ed5
Christopher Sardegna <github@reagentx.net>
2018-12-25 12:06:29 -0800

    Add more example code

--- scripts/run.py
+++ scripts/run.py
@@ -1,10 +1,16 @@
 from purpleair import purpleair
+from purpleair import sensor
 
 
 p = purpleair.PurpleAir()
 print(len(p.useful_sensors))
 s = p.useful_sensors[0]
 s.get_field('field2')
-s.get_location()
-# print(s.thingspeak_data)
+# s.get_location()
 print(s)
+
+se = sensor.Sensor('2891', parse_location=True)
+se.get_field('field3')
+se.get_field('field4')
+print(se.thingspeak_data.keys())
+print(se)
