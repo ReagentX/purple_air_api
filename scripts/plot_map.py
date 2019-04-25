70bb6fd3565c842d1bd2c681622f4b63af075756
Christopher Sardegna <github@reagentx.net>
2019-04-24 21:13:49 -0700

    Add formatting for Jupyter

--- scripts/plot_map.py
+++ scripts/plot_map.py
@@ -1,3 +1,4 @@
+#%%
 from mpl_toolkits.basemap import Basemap
 import numpy as np
 import matplotlib.pyplot as plt
@@ -7,6 +8,7 @@ import datetime
 import random
 
 
+#%%
 # Get the purpleair data
 p = purpleair.PurpleAir()
 df = p.to_dataframe('all')
@@ -15,8 +17,10 @@ var_to_viz = 'temp_c'  # The dict item that we want to visualize
 lat = df['lat'].values
 lon = df['lon'].values
 colors = df[var_to_viz].values  # Variable on which to generate the color gradient
-# print(min(colors), max(colors))
+print(min(colors), max(colors))
 
+
+#%%
 margin = 0  # buffer to add to the range
 lat_min = min(lat) - margin
 lat_max = max(lat) + margin
@@ -33,21 +37,24 @@ m = Basemap(llcrnrlon=lon_min,
             resolution = 'h',
             area_thresh=10000.,
             )
-
 m.drawcoastlines()
 m.drawcountries()
 m.drawstates()
 
+
+#%%
 # Colors
 water = '#46bcec'
 land = '#ffffff'
 m.drawmapboundary(fill_color=water)
 m.fillcontinents(color = land, lake_color=water)
 
+
+#%%
 # convert lat and lon to map projection coordinates
 lons, lats = m(lon, lat)
 # plot points as red dots
 m.scatter(lons, lats, marker='o', c=colors, cmap='plasma', zorder=5, s=2)
 plt.colorbar().set_label(f'{var_to_viz}', rotation=270)
 plt.title(f'Global {var_to_viz} at {datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")}')
-plt.savefig('maps/sensor_map.png', dpi=300)
+# plt.savefig('maps/sensor_map.png', dpi=300)
