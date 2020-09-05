6d9c177bcd0703d0974ae77c58d48ef366c020f8
Christopher Sardegna <gpg@reagentx.net>
2020-09-04 23:47:23 -0400

    Formatting

--- purpleair/purpleair.py
+++ purpleair/purpleair.py
@@ -1,21 +1,25 @@
 import json
+from datetime import timedelta
+
 import pandas as pd
 import requests
 import requests_cache
-from datetime import timedelta
+
 from .api_data import API_ROOT
 from .sensor import Sensor
-
 
 # Setup cache for requests
 requests_cache.install_cache(expire_after=timedelta(hours=1))
 requests_cache.core.remove_expired_responses()
 
+
 class PurpleAir():
     def __init__(self, parse_location=False):
         self.data = self.get_all_data()
-        self.all_sensors = [Sensor(s['ID'], json=s, parse_location=parse_location) for s in self.data]
-        self.outside_sensors = [s for s in self.all_sensors if s.location_type == 'outside']
+        self.all_sensors = [
+            Sensor(s['ID'], json=s, parse_location=parse_location) for s in self.data]
+        self.outside_sensors = [
+            s for s in self.all_sensors if s.location_type == 'outside']
         self.useful_sensors = [s for s in self.all_sensors if s.is_useful()]
 
     def get_all_data(self) -> dict:
@@ -25,7 +29,6 @@ class PurpleAir():
         print(f"Initialized {len(data['results']):,} sensors!")
         return data['results']
 
-    
     def to_dataframe(self, sensor_group: str) -> pd.DataFrame:
         '''Converts dictionary representation of a list of sensors to a Pandas Dataframe
         where sensor_group determines which group of sensors are used'''
