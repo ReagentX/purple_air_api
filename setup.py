05d0e7d66bcb990a3f48d5d0db63efbfc6566973
Christopher Sardegna <gpg@reagentx.net>
2020-09-05 09:51:00 -0400

    Make real python package

--- setup.py
+++ setup.py
@@ -1,11 +1,28 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
+with open("README.md", "r") as fh:
+    LONG_DESCRIPTION = fh.read()
 
 setup(
-    name='purpleair_api',
-    version='1.0',
-    description='A Python 3.7 PurpleAir API Class.',
+    name='purpleair',
+    version='1.0.1',
+    description='Python API Client to get and transform PurpleAir data.',
+    long_description=LONG_DESCRIPTION,
+    long_description_content_type="text/markdown",
     author='Christopher Sardegna',
     author_email='github@reagentx.net',
-    scripts=['scripts/run.py']
+    url='https://github.com/ReagentX/purple_air_api/',
+    packages=find_packages(),
+    install_requires=['requests', 'requests_cache', 'thingspeak', 'geopy', 'pandas'],
+    python_requires='>=3.6',
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)',
+        'Natural Language :: English',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 3.8',
+        'Intended Audience :: Science/Research',
+        'Topic :: Scientific/Engineering :: Information Analysis',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+    ]
 )
