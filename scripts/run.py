f476987bc6d4d87d6de687aedef0bc58662aedfc
Christopher Sardegna <gpg@reagentx.net>
2020-09-07 00:31:53 -0400

    Update imports in script

--- scripts/run.py
+++ scripts/run.py
@@ -1,9 +1,9 @@
-from purpleair import purpleair
-from purpleair import sensor
+from purpleair.purpleair import PurpleAir
+from purpleair.sensor import Sensor
 
 
 # All Sensors
-p = purpleair.PurpleAir()
+p = PurpleAir()
 print(len(p.useful_sensors))
 s = p.useful_sensors[0] # First confirmed useful sensor
 s.get_location()
@@ -12,7 +12,7 @@ df = p.to_dataframe('useful')
 print(df.head())
 
 # Single sensor
-se = sensor.Sensor('2891', parse_location=True)
+se = Sensor('2891', parse_location=True)
 se.get_field('field3')
 se.get_field('field4')
 print(se.thingspeak_data.keys())
