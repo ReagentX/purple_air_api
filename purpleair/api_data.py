093042c198da7bb4097a4d8a91e9d92ae4a40813
Christopher Sardegna <gpg@reagentx.net>
2020-09-11 17:11:32 -0400

    Move cache logic to one file

--- purpleair/api_data.py
+++ purpleair/api_data.py
@@ -1,5 +1,17 @@
 """
 Constants for PurpleAir API
 """
+from datetime import timedelta
+from sqlite3 import OperationalError
+
+import requests_cache
+
+# Setup cache for requests
+requests_cache.install_cache(expire_after=timedelta(hours=1))
+try:
+    requests_cache.core.remove_expired_responses()
+except OperationalError:
+    print('Unable to purge cache database, cache may contain old data!!!')
+
 
 API_ROOT = 'https://www.purpleair.com/json'
