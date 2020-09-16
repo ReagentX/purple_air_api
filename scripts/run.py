75fe53d7f04bf2a79699a31c2d5a3725bfbffea6
Christopher Sardegna <gpg@reagentx.net>
2020-09-16 16:35:43 -0400

    Update sample script code

--- scripts/run.py
+++ scripts/run.py
@@ -11,9 +11,9 @@ from purpleair.sensor import Sensor
 # All Sensors
 p = SensorList()
 print(len(p.useful_sensors))
-s = p.useful_sensors[0] # First confirmed useful sensor
+s = p.useful_sensors[0]  # First confirmed useful sensor
 s.get_location()
-df = p.to_dataframe(sensor_filter='outside', channel='a')
+df = p.to_dataframe(sensor_filter='all', channel='a')
 print(df.head())
 
 # Single sensor
@@ -25,6 +25,7 @@ print(se.as_flat_dict('a'))
 se.get_field('field3')
 se.get_field('field4')
 print(se.thingspeak_data.keys())
-print(se.channel_a.get_historical(weeks_to_get=1,
-                                  sensor_channel='a',
-                                  thingspeak_field='secondary'))
+df = se.channel_a.get_historical(weeks_to_get=1,
+                                 sensor_channel='a',
+                                 thingspeak_field='secondary')
+print(df.head())
