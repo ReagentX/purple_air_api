eb24e21048ece46628cdd5ef782c3532f598af7c
Christopher Sardegna <gpg@reagentx.net>
2020-09-16 16:26:15 -0400

    Update run script for new channel methods

--- scripts/run.py
+++ scripts/run.py
@@ -3,6 +3,7 @@ Sample script to run quick tests
 """
 
 
+from purpleair import channel
 from purpleair.network import SensorList
 from purpleair.sensor import Sensor
 
@@ -12,13 +13,18 @@ p = SensorList()
 print(len(p.useful_sensors))
 s = p.useful_sensors[0] # First confirmed useful sensor
 s.get_location()
-print(s.as_flat_dict())
-df = p.to_dataframe('useful')
+print(s)
+print(s.as_flat_dict('a'))
+df = p.to_dataframe(sensor_filter='outside', channel='a')
 print(df.head())
 
 # Single sensor
-se = Sensor('2891')
+se = Sensor('2890')
+print(se.channel_a)
+print(se.channel_b)
 se.get_field('field3')
 se.get_field('field4')
 print(se.thingspeak_data.keys())
-print(se.get_historical(weeks_to_get=1, sensor_channel='a'))
+print(se.channel_a.get_historical(weeks_to_get=1,
+                                  sensor_channel='a',
+                                  thingspeak_field='secondary'))
