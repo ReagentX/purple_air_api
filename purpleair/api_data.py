595ae592f57cd143015514fb22b45bd4cd250e21
Christopher Sardegna <gpg@reagentx.net>
2020-09-16 16:25:12 -0400

    Add constants for column names

--- purpleair/api_data.py
+++ purpleair/api_data.py
@@ -16,3 +16,55 @@ except OperationalError:
 
 
 API_ROOT = 'https://www.purpleair.com/json'
+
+CHANNEL_A_PRIMARY_COLS = {
+    'created_at': 'created_at',
+    'entry_id': 'entry_id',
+    'field1': 'PM1.0_CF_ATM_ug/m3',
+    'field2': 'PM2.5_CF_ATM_ug/m3',
+    'field3': 'PM10.0_CF_ATM_ug/m3',
+    'field4': 'UptimeMinutes',
+    'field5': 'ADC',
+    'field6': 'Tempertautre_F',
+    'field7': 'Humidity_%',
+    'field8': 'PM2.5_CF_1_ug/m3',
+}
+
+CHANNEL_A_SECONDARY_COLS = {
+    'created_at': 'created_at',
+    'entry_id': 'entry_id',
+    'field1': '0.3um/dl',
+    'field2': '0.5um/dl',
+    'field3': '1.0um/dl',
+    'field4': '2.5um/dl',
+    'field5': '5.0um/dl',
+    'field6': '10.0um/dl',
+    'field7': 'PM1.0_CF_1_ug/m3',
+    'field8': 'PM10.0_CF_1_ug/m3',
+}
+
+CHANNEL_B_PRIMARY_COLS = {
+    'created_at': 'created_at',
+    'entry_id': 'entry_id',
+    'field1': 'PM1.0_CF_ATM_ug/m3',
+    'field2': 'PM2.5_CF_ATM_ug/m3',
+    'field3': 'PM10.0_CF_ATM_ug/m3',
+    'field4': 'UptimeMinutes',
+    'field5': 'RSSI_dbm',
+    'field6': 'Pressure_hpa',
+    'field7': 'Blank',
+    'field8': 'PM2.5_CF_1_ug/m3',
+}
+
+CHANNEL_B_SECONDARY_COLS = {
+    'created_at': 'created_at',
+    'entry_id': 'entry_id',
+    'field1': '0.3um/dl',
+    'field2': '0.5um/dl',
+    'field3': '1.0um/dl',
+    'field4': '2.5um/dl',
+    'field5': '5.0um/dl',
+    'field6': '10.0um/dl',
+    'field7': 'PM1.0_CF_1_ug/m3',
+    'field8': 'PM10.0_CF_1_ug/m3'
+}
