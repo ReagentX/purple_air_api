49fc042761681d7c79b4b7bd0ad79402a8517e07
Christopher Sardegna <gpg@reagentx.net>
2020-09-16 23:28:46 -0400

    Update get_historical sample

--- scripts/run.py
+++ scripts/run.py
@@ -24,6 +24,5 @@ se.get_field('field3')
 se.get_field('field4')
 print(se.thingspeak_data.keys())
 df = se.parent.get_historical(weeks_to_get=1,
-                              sensor_channel='a',
                               thingspeak_field='secondary')
 print(df.head())
